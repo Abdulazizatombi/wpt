c34bceb579189849e9b495820d5c2fd4532cfa76
Yoav Weiss <yoavweiss@chromium.org>
2020-05-07 07:08:16 -0700

    [Client-Hints] Fix cross-origin redirect leak
    Client Hints are not supposed to be sent across cross-origin
    redirects unless FeaturePolicy indicates that they should.
    This CL enforces that and adds tests to that effect.
    
    Bug: 911952
    Change-Id: If3453409385b50f84b7ae188965b81c24f87dfc8

--- client-hints/accept-ch-stickiness/resources/accept-ch-test.js
+++ client-hints/accept-ch-stickiness/resources/accept-ch-test.js
@@ -19,6 +19,21 @@ function verify_initial_state(initial_url, test_name) {
     "hints preferences cached");
 }
 
+function verify_iframe_state(expect_url, test_name) {
+  promise_test(t => {
+    return new Promise(resolve => {
+      window.addEventListener('message', t.step_func(function(e) {
+        assert_equals(e.data, "PASS", "message from opened frame");
+        fetch("/client-hints/accept-ch-stickiness/resources/clear-site-data.html").then(resolve);
+        resolve();
+      }));
+      const iframe = document.createElement("iframe");
+      iframe.src = expect_url;
+      document.body.appendChild(iframe);
+    });
+  }, test_name + " got client hints according to expectations.");
+}
+
 function verify_navigation_state(expect_url, test_name) {
   promise_test(t => {
     return new Promise(resolve => {
@@ -37,15 +52,10 @@ function verify_navigation_state(expect_url, test_name) {
 
 function verify_subresource_state(expect_url, test_name) {
   promise_test(t => {
-    return new Promise(resolve => {
-      let win;
-      window.addEventListener('message', t.step_func(function(e) {
-        win.close();
-        assert_equals(e.data, "PASS", "message from opened page");
-        fetch("/client-hints/accept-ch-stickiness/resources/clear-site-data.html").then(resolve);
-      }));
-      // Open expect_url as a subresource.
-      fetch(expect_url).then(resolve);
+    return fetch(expect_url).then(response => {
+      return response.text();
+    }).then(text => {
+      assert_true(text.includes("PASS"));
     });
   }, test_name + " got client hints according to expectations.");
 }
